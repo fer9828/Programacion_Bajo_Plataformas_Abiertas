446097da39c2a746a363aadf1a27e9c26465e3d2
Fernando Jiménez Ureña <95489667+fer9828@users.noreply.github.com>
2022-04-26 22:08:11 -0600

    Final Project

---
+++ Proyecto Plataformas/música opcional/music.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("C:/Users/Usuario/Downloads/Pacman_Complete/Pacman_Complete/pacman.mp3")
+pygame.mixer.music.play(1)
+pygame.mixer.music.set_volume(1.0)
+
+
